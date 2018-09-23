ea909b674d90ddf77607bf68da4e0e4ce86205e7
Dida Handian <handiandida@gmail.com>
2018-09-23 19:13:46 +0700

    Biodata

---
+++ src/pbo3/pkg10117091/latihan1/biodata/PBO310117091Latihan1Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pbo3.pkg10117091.latihan1.biodata;
+
+/* @author
+* NAMA  : DIDA HANDIAN
+* KELAS : PBO3
+* NIM   : 10117091
+* Deskripsi Program : Membuat dan Menampilkan Biodata
+*/
+
+public class PBO310117091Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Dida Handian");
+        System.out.println("ALAMAT\t\t: Jl.Sumbawa No.110");
+        System.out.println("TEMPAT LAHIR\t: Tasikmlaya");
+        System.out.println("TANGGAL LAHIR\t: 22 Mei 1998");
+        System.out.println("---------------------------");
+        System.out.println("HOBI\t\t: Bermain");
+        System.out.println("MAKES\t\t: Bakso");
+        System.out.println("MIKES\t\t: Teh");
+    }
+    
+}
